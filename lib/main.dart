408cba44f15a6fceea541045455d8dc444d988da
tri yulianto <threeyulianto333@gmail.com>
2021-03-23 08:59:34 +0700

    page login selesai

--- lib/main.dart
+++ lib/main.dart
@@ -66,13 +66,34 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            Stack(children: <Widget>[
-              SvgPicture.asset("lib/assets/images/login/bg_wave1_biru.svg"),
-              Padding(
-                  padding: const EdgeInsets.all(100),
-                  child: (SvgPicture.asset(
-                      "lib/assets/images/login/img_logo.svg"))),
-            ]),
+            SizedBox(
+              width: double.infinity,
+              child: Stack(children: <Widget>[
+                SizedBox(
+                    width: double.infinity,
+                    child: SvgPicture.asset(
+                        "lib/assets/images/login/bg_wave1_biru.svg")),
+                Padding(
+                    padding: const EdgeInsets.all(100),
+                    child: Column(children: <Widget>[
+                      SvgPicture.asset("lib/assets/images/login/img_logo.svg"),
+                      Text(
+                        "Presensi",
+                        style: TextStyle(
+                            fontFamily: 'Nunito',
+                            fontSize: 40,
+                            color: Colors.white),
+                      ),
+                      Text(
+                        "Staff",
+                        style: TextStyle(
+                            fontFamily: 'Nunito',
+                            fontSize: 25,
+                            color: Colors.white),
+                      )
+                    ])),
+              ]),
+            ),
             Padding(
               padding: const EdgeInsets.only(left: 50, right: 50),
               child: Column(children: <Widget>[
@@ -91,25 +112,30 @@ class _MyHomePageState extends State<MyHomePage> {
                         border: OutlineInputBorder()),
                   ),
                 ),
-                Container(
-                  margin: const EdgeInsets.only(top: 20, bottom: 100),
-                  child: TextButton(
-                      child: Text("Login".toUpperCase(),
-                          style: TextStyle(fontSize: 20)),
-                      style: ButtonStyle(
-                          padding: MaterialStateProperty.all<EdgeInsets>(
-                              EdgeInsets.all(15)),
-                          foregroundColor:
-                              MaterialStateProperty.all<Color>(Colors.white),
-                          backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.green),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ))),
-                      onPressed: () => null),
+                SizedBox(
+                  width: double.infinity,
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 50, bottom: 20),
+                    child: TextButton(
+                        child: Text("Login".toUpperCase(),
+                            style: TextStyle(fontSize: 20)),
+                        style: ButtonStyle(
+                            padding: MaterialStateProperty.all<EdgeInsets>(
+                                EdgeInsets.all(15)),
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.green),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ))),
+                        onPressed: () => null),
+                  ),
                 ),
+                Container(
+                    padding: const EdgeInsets.only(bottom: 100),
+                    child: Text("Lupa akun ?"))
               ]),
             )
           ],
